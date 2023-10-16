d53229ec52d2aed4a5adec98364bd65b2ed2c5db
Cimarron <cimarron13@mac.com>
2023-10-15 21:03:30 -0500

    server.js code

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process. env.PORT || 3000;
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// Serve static files
+app.use(express.static('public'));
+
+// HTML routes
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// API routes
+// Add API routes here
+
+//Start the server
+app.listen(PORT, ()=>{
+    console.log (`Server running on HTTP://localhost:${PORT}`)
+});
