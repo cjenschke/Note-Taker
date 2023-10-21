986026063bdecc2868b1ed58b9e12d7d1ecc31c4
cjenschke <cimarron13@mac.com>
2023-10-21 16:24:27 -0500

    fixed file past to notes

--- Develop/server.js
+++ Develop/server.js
@@ -21,32 +21,47 @@ app.listen(PORT, () => {
 // Route setups
 
 // Route for serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(__dirname, '/public/index.html');
+app.get('/', (req, res) => {
+  console.log('Received request for index.html');
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // Route for serving the notes.html file
 app.get('/notes', (req, res) => {
+  console.log('Received request for notes.html');
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // Route to read all saved notes
-app.get('public/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
+  console.log('Received request for /api/notes');
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log('Sending JSON response for /api/notes');
     res.json(JSON.parse(data));
   });
 });
 
 // Route to save a new note
 app.post('/api/notes', (req, res) => {
+  console.log('Received POST request for /api/notes');
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      throw err;
+    }
     const notes = JSON.parse(data);
     const newNote = { ...req.body, id: uuidv4() };
     notes.push(newNote);
     fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
-      if (err) throw err;
+      if (err) {
+        console.error(err);
+        throw err;
+      }
+      console.log('Note saved successfully');
       res.json(newNote);
     });
   });
@@ -56,14 +71,21 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      throw err;
+    }
     let notes = JSON.parse(data);
     const updatedNotes = notes.filter((note) => note.id !== noteId);
     fs.writeFile(
       './db/db.json',
       JSON.stringify(updatedNotes, null, 2),
       (err) => {
-        if (err) throw err;
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+        console.log('Note deleted successfully');
         res.json({ message: 'Note deleted successfully' });
       }
     );
