f3d32b2c855e8aa13f41600f2caf70b88dfb2786
cjenschke <cimarron13@mac.com>
2023-10-21 16:38:23 -0500

    fixed typo that caused me hours of headache

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -12,7 +12,7 @@ if (window.location.pathname === '/notes') {
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   clearBtn = document.querySelector('.clear-btn');
-  noteList = document.querySelectorAll('.list-container.list-group');
+  noteList = document.querySelectorAll('.list-container .list-group');
 }
 
 // Show an element
