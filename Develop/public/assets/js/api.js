706ec6b1bbc2fef0622aeae8c1b68de05751c178
cjenschke <cimarron13@mac.com>
2023-10-21 09:33:08 -0500

    added api.js code

---
+++ Develop/public/assets/js/api.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// Retrieve all notes
+router.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err),
+        res.status(500).json({ error: 'Failed to retrieve notes' });
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+// Save a new note
+router.post('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to save note' });
+    } else {
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = Date.now().toString();
+      notes.push(newNote);
+      fs.writeFile(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to save note' });
+          } else {
+            res.json(newNote);
+          }
+        }
+      );
+    }
+  });
+});
+
+// Delete a note
+router.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to delete note' });
+    } else {
+      const notes = JSON.parse(data);
+      const updateNotes = notes.filter((note) => note.id !== noteId);
+      fs.writeFile(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(updatedNotes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to delete note' });
+          } else {
+            res.sendStatus(204);
+          }
+        }
+      );
+    }
+  });
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,20 +7,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Serve static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 // app.use(express.static(path.join(__dirname, "public")));
 
 // HTML routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // API routes
-// Add API routes here
+app.use('/api', require('./public/assets/js/api'));
 
 //Start the server
 app.listen(PORT, () => {
